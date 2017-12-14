8597ac1cf2ea0504a1bd7c8e41b1ab11b777e3e3
PEDRO DE LA CRUZ MARTINEZ <pcruz9332@gmail.com>
2017-12-13 20:46:47 -0800

    Movie thing is done, the only problem is the music thing

--- liri.js
+++ liri.js
@@ -2,6 +2,8 @@ var input = process.argv
 
 var user = input[2];
 
+var request = require("request");
+
 var holder = require("./keys.js");
 
 var Twitter = require("twitter");
@@ -9,6 +11,13 @@ var client = new Twitter(holder.twitterKeys);
 
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(holder.spotifyKeys);
+//the loop that takes the information after the command
+var userInput = [];
+	for(var i = 3; i < input.length; i++) {
+		userInput.push(input[i]);
+		//console.log(song);
+	}
+	var name = userInput.slice(0, userInput.length - 1).join(" ") + " " + userInput.slice(-1);
 
 if (user === "my-tweets") {
   var parameters = {screen_name: "nodejs"};
@@ -20,22 +29,32 @@ if (user === "my-tweets") {
     }
   });
 } else if (user === "spotify-this-song") {
-	var song = [];
-	for(var i = 3; i < input.length; i++) {
-		song.push(input[i]);
-		//console.log(song);
-	}
-	var name = song.slice(0, song.length - 1).join(" ") + " " + song.slice(-1);
-	//console.log("Your song name is: " + name);
 	spotify.search({ type: 'track',
 					 query: name,
 					 limit: 1}, function(err, data) {
 							  if (err) {
 							    return console.log('Error occurred: ' + err);
 							  }
-							var dataElm = JSON.stringify(data.tracks, null, 2);
+							var dataElm = JSON.stringify(data.tracks.items, null, 2);
 							console.log(dataElm); 
 							});
-}else {
+}else if (user === "movie-this") {
+	var queryUrl = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy";
+
+	request(queryUrl, function(error, response, body) {
+
+	  // If the request is successful
+	  if (!error && response.statusCode === 200) {
+	  	//console.log(JSON.parse(body))
+	  	console.log("Movie Name: " + name);
+	    console.log("Release Year: " + JSON.parse(body).Year);
+	    console.log("IMBD Rating: " + JSON.parse(body).imdbRating);
+	    console.log("Language: " + JSON.parse(body).Language);
+	    console.log("Plot: " + JSON.parse(body).Plot);
+	    console.log("Actors: " + JSON.parse(body).Actors);
+	  }
+	});
+}
+else {
 	console.log("This didnt work!");
 }
